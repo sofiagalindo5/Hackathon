9390a4ca67579f92f2bcda5a2c6f0c24e45f8234
Rasmin Ahmed <rasminahmed@email.com>
2026-02-08 00:24:02 -0500

    changed the titles of tab 1 and tab 2

--- notes-app/app/_layout.tsx
+++ notes-app/app/_layout.tsx
@@ -13,10 +13,6 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "login",
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -51,6 +47,10 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen
+         name="login"
+         options={{ headerShown: false }}
+       />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
