b9ec094e6957f20f9f13b6e80ea2d6cd4dadd396
MatthewJung14 <Matthewjung14@gmail.com>
2026-02-08 00:02:10 -0500

    Database Setup

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.database import db 
+
+from db import db
 
 # IMPORTANT: import the router
 from routes.upload_routes import router as upload_router
